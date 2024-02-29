2d426f969fae09d02e9c5bf20015b0d2038d1c43
Sina Nasiri <sina.nasiri@datavirtuality.de>
2024-02-29 19:30:54 +0330

    Added base files

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,72 @@
-use std::io::{stdout, Write};
+use std::{io::{stdout, Stdout, Write}, time::Duration};
 
 use crossterm::{
-    execute,
-    style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor},
-    ExecutableCommand,
-    event,
+    cursor::MoveTo, event::{self, poll, read, Event, KeyCode}, execute, style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor}, terminal::{disable_raw_mode, enable_raw_mode, size}, ExecutableCommand, QueueableCommand
 };
 
+struct World{
+    player_c: u16,
+    player_l: u16,
+}
+
+fn draw(mut sc: &Stdout ,world: &World){
+    sc.queue(MoveTo(world.player_c, world.player_l));
+    sc.queue(Print("p"));
+
+    sc.flush();
+}
+
 fn main() -> std::io::Result<()> {
-    // // using the macro
-    // execute!(
-    //     stdout(),
-    //     SetForegroundColor(Color::Blue),
-    //     SetBackgroundColor(Color::Red),
-    //     Print("Styled text here."),
-    //     ResetColor
-    // )?;
 
-    // or using functions
 
+    // init the screen
     let mut sc = stdout();
+    let (maxc , maxl) = size().unwrap();
 
+    enable_raw_mode();
     
     sc
-        .execute(SetForegroundColor(Color::Blue))?
-        .execute(SetBackgroundColor(Color::Red))?
-        .execute(Print("Styled text here."))?
+        .execute(SetBackgroundColor(Color::Green))?
+        // .execute(Print("Styled text here."))?
         .execute(ResetColor)?;
+
+    // init the game
+    let mut world = World {
+        player_c: 0, player_l: maxl / 2
+    };
+
     
+
+    loop {
+    if poll(Duration:: from_millis(10))? {
+let key = read().unwrap();
+
+match key {
+    Event::Key(event) => {
+        match event.code {
+            KeyCode::Enter => { 
+                // todo change bird position
+                // if world.player_c >= 0 {world.player_c += 4};
+                // if world.player_l > maxl-4 {world.player_l +=4};
+            },
+            KeyCode::End => {
+                break;
+            }
+            _ => {}
+        }
+    },
+    _ => {}
+}
+    }else{
+
+    }
+
+    // physics()
+
+    draw(&sc, &world);
+
+    };
+
+    disable_raw_mode();
     Ok(())
 }
